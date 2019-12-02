fa963e6e541907ebd0453e94a212cca1769ef675
JesalRajendra.Shah <jesalrajendra.shah@cognizant.com>
2019-12-02 14:41:00 +0000

    reverted

--- keepnote-step6-boilerplate/CategoryService/Startup.cs
+++ keepnote-step6-boilerplate/CategoryService/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace CategoryService
